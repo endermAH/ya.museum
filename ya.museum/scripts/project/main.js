d8b6d5d4f80004e9ac408d2886fababb2bc9b235
Evgeny Kuryatov <evgeny.kuryatov@netcracker.com>
2024-06-25 15:55:49 +0500

    Update

---
+++ ya.museum/scripts/project/main.js
@@ -0,0 +1,36 @@
+
+// Import any other script files here, e.g.:
+// import * as myModule from "./mymodule.js";
+  // Import the functions you need from the SDKs you need
+
+  
+  
+// (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+// new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+// j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+// 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+// })(window,document,'script','dataLayer','GTM-PFSSDJ8R');
+
+
+
+runOnStartup(async runtime =>
+{
+	// Code to run on the loading screen.
+	// Note layouts, objects etc. are not yet available.
+	
+	runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime));
+});
+
+async function OnBeforeProjectStart(runtime)
+{
+	// Code to run just before 'On start of layout' on
+	// the first layout. Loading has finished and initial
+	// instances are created and available to use here.
+	
+	runtime.addEventListener("tick", () => Tick(runtime));
+}
+
+function Tick(runtime)
+{
+	// Code to run every tick
+}
---
+++ ya.museum/scripts/project/scriptsInEvents.js
@@ -0,0 +1,86 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAnalytics, logEvent, isSupported } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBYTVVsTsXnej03p2OsVqpybkYz4mKzSKY",
+	authDomain: "yagodnoe-digital-museum.firebaseapp.com",
+	projectId: "yagodnoe-digital-museum",
+	storageBucket: "yagodnoe-digital-museum.appspot.com",
+	messagingSenderId: "1010874666524",
+	appId: "1:1010874666524:web:44e5e43c9300d9c170d9d9",
+	measurementId: "G-4B87NRZF3N"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function LogBuildingCheck(building_id) {
+	logEvent(analytics, "select_content", {
+	  content_type: "building",
+	  content_id: building_id
+	})
+}
+
+function LogSelectContent(content_type, content_id) {
+	logEvent(analytics, "select_content", {
+	  content_type: content_type,
+	  content_id: content_id
+	})
+}
+
+function LogMapActivity(activity_type) {
+	logEvent(analytics, "select_content", {
+	  content_type: "map_activity",
+	  content_id: activity_type
+	})
+}
+
+function LogHouseCheck(house_name) {
+	logEvent(analytics, "show_house", {
+	  house_name: house_name
+	})
+}
+
+
+const scriptsInEvents = {
+
+	async Es_main_Event13_Act2(runtime, localVars)
+	{
+		LogSelectContent("show", "book")
+	},
+
+	async Es_main_Event14_Act2(runtime, localVars)
+	{
+		LogSelectContent("show", "gallery")
+	},
+
+	async Es_main_Event15_Act2(runtime, localVars)
+	{
+		LogSelectContent("show", "age_story")
+	},
+
+	async Es_main_Event16_Act2(runtime, localVars)
+	{
+		LogSelectContent("show", "video")
+	},
+
+	async Es_main_Event18_Act1(runtime, localVars)
+	{
+		LogMapActivity("sound")
+	},
+
+	async Es_main_Event21_Act2(runtime, localVars)
+	{
+		LogMapActivity("story")
+	},
+
+	async Es_main_Event28_Act8(runtime, localVars)
+	{
+		LogBuildingCheck(localVars.house_id)
+		LogHouseCheck(runtime.callFunction("get_hause_field", localVars.house_id, "name"))
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
